4c2360b520fa4953ca335723121fc188df400731
dsmirnov <dsmirnov@s-pro.io>
2019-12-27 18:14:40 +0200

    added test

--- src/test/java/com/full/circle/registration/restjwtpostgres/service/UserServiceTest.java
+++ src/test/java/com/full/circle/registration/restjwtpostgres/service/UserServiceTest.java
@@ -1,21 +1,45 @@
 package com.full.circle.registration.restjwtpostgres.service;
 
+import com.full.circle.registration.restjwtpostgres.dto.UserDTO;
+import com.full.circle.registration.restjwtpostgres.model.User;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class UserServiceTest {
+    private UserDTO user;
 
     @Autowired
     private AuthenticationService authenticationService;
 
+    @Before
+    public void initUser(){
+        user = new UserDTO();
+        user.setUsername("test");
+        user.setEmail("test@r.com");
+        user.setPassword("12345678");
+    }
+
     @Test
-    public void addUser(){
-        Assert.assertTrue(true);
+    public void signUp(){
+       ResponseEntity response = authenticationService.signUp(user);
+
+//       Assert.assertTrue();
+    }
+
+    @After
+    public void deleteUser(){
+
     }
 }
